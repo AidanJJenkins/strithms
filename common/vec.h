8b12cd4a796d2b449a8e557b0370dd491e4814e6
Aidan Jenkins <aidan.jenkins@gmail.com>
2024-06-23 20:25:06 -0700

    more

--- common/vec.h
+++ common/vec.h
@@ -12,7 +12,6 @@ vec *new_vec();
 void resize_vec(vec *arr);
 void push(vec *arr, int item);
 int pop(vec *arr);
-int get_at(vec *arr, int idx);
 void del_at(vec *arr, int idx);
 void del_vec(vec *arr);
 
